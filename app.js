92c4fce8f366d4eee73a61aeec3c6c69d57031cf
deenapoulose <deenapoulose123@gmail.com>
2021-04-13 13:34:55 +0530

    first

--- app.js
+++ app.js
@@ -38,14 +38,52 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/',(req,res)=>{
     //res.send('hii')
-    let sql ='select * from tasktable';
+    let sql ='SELECT * FROM tasktable';
     let query= connection.query(sql,(err,rows)=>{
+        
        if(err)throw err; 
        res.render('user_index',{
         tittle:'curd',
-        // user:
-    })
+        task:rows
+    });
     
+    });
+});
+app.post('/save',(req,res)=>{
+    let data={taskname: req.body.t};
+    let sql1="INSERT INTO tasktable SET ?;"
+    let query1=connection.query(sql1,data,(err,results)=>{
+         if(err) throw err;
+         res.redirect('/');
+     });
+});
+app.get('/edit/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql2 = `Select * from tasktable where id = ${userId}`;
+    let query2 = connection.query(sql2,(err, result) => {
+        if(err) throw err;
+        res.render('user_edit', {
+            title : 'edit',
+            task : result[0]
+        });
+    });
+});
+app.post('/update',(req,res)=>{
+    
+    const userId = req.body.id;
+    let sql2 = "update tasktable  SET taskname='"+req.body.t+"'   where id ="+userId;
+    
+    let query2=connection.query(sql2,(err,results)=>{
+         if(err) throw err;
+         res.redirect('/');
+     });
+});
+app.get('/delete/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql3 = `DELETE from tasktable where id = ${userId}`;
+    let query3= connection.query(sql3,(err, result) => {
+        if(err) throw err;
+        res.redirect('/');
     });
 });
 // Server Listening
